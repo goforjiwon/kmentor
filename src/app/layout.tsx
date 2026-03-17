00c55c0829289290257de1ac7e6395a27472eda0
goforjiwon <103751687+goforjiwon@users.noreply.github.com>
2026-03-17 14:52:06 +0900

    Add Analytics component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 const notoSansKr = Noto_Sans_KR({
   subsets: ["latin"],
@@ -19,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body className={notoSansKr.className}>{children}</body>
+      <body className={notoSansKr.className}>{children}<Analytics /></body>
     </html>
   );
 }
